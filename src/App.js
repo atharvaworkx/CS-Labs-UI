d88579976a530fe3dd98874c7acd78c8e6a5d1c1
atharvaworkx <atharva.workx@gmail.com>
2026-06-29 16:32:36 +0530

    Refactor: updated contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -18,8 +20,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/courses" element={<Dashboard />} />
-              <Route path="/blog" element={<Dashboard />} />
-              <Route path="/contact" element={<Dashboard />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/contact" element={<Contact />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/profile" element={<Dashboard />} />
               <Route path="/my-courses" element={<Dashboard />} />
